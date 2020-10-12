8f8b0febff3567adda66c866239eb554e49dadcb
Alexander Thaller <alexander.thaller@trivago.com>
2020-10-12 13:58:52 +0200

    The init zsh file shoudnt use fullpath

--- resources/init.zsh
+++ resources/init.zsh
@@ -1,17 +1,17 @@
 function histdb-rs-init() {
   local session_id;
-  session_id="$(/home/athaller/.cargo/bin/histdb-rs session_id)"
+  session_id="$(histdb-rs session_id)"
   export HISTDB_RS_SESSION_ID="${session_id}"
 }
 
 function histdb-rs-zshaddhistory() {
   unset HISTDB_RS_RETVAL;
-  /home/athaller/.cargo/bin/histdb-rs zshaddhistory $@
+  histdb-rs zshaddhistory $@
 }
 
 function histdb-rs-precmd() {
   export HISTDB_RS_RETVAL="${?}"
-  /home/athaller/.cargo/bin/histdb-rs precmd
+  histdb-rs precmd
 }
 
 add-zsh-hook zshaddhistory histdb-rs-zshaddhistory
