f8fe7831e3c66b0ebd2b46ba44c3c14b7fa92f2b
Alexander Thaller <alexander.thaller@trivago.com>
2022-07-04 10:35:10 +0200

    Implement text_excluded flag (#41)
    Added following flag:
    ```
        -T, --text_excluded <command-text-excluded>
                Only print entries not containing the given regex
    ```
    
    When specified it will exclude all commands containing the given regex.
    
    Example:
    
    ```
    » echo 'test'
    test
    
    » hstdb
     tmn    cmd
     10:19  echo 'test'
    
    » hstdb -T test
     tmn    cmd
    ```

--- src/store/filter.rs
+++ src/store/filter.rs
@@ -19,6 +19,7 @@ pub struct Filter {
     pub command: Option<String>,
     pub no_subdirs: bool,
     pub command_text: Option<Regex>,
+    pub command_text_excluded: Option<Regex>,
     pub count: usize,
     pub session: Option<Regex>,
     pub filter_failed: bool,
@@ -68,10 +69,16 @@ impl Filter {
         Self { count, ..self }
     }
 
-    pub fn command(self, command: Option<String>, command_text: Option<Regex>) -> Self {
+    pub fn command(
+        self,
+        command: Option<String>,
+        command_text: Option<Regex>,
+        command_text_excluded: Option<Regex>,
+    ) -> Self {
         Self {
             command,
             command_text,
+            command_text_excluded,
             ..self
         }
     }
@@ -97,6 +104,11 @@ impl Filter {
                 self.command_text
                     .as_ref()
                     .map_or(true, |regex| regex.is_match(&entry.command))
+            })
+            .filter(|entry| {
+                self.command_text_excluded
+                    .as_ref()
+                    .map_or(true, |regex| !regex.is_match(&entry.command))
             })
             .filter(|entry| {
                 self.session
